5063a262b4ba5f75f2a7aee77e079eaf7c29dcb4
fontesLarissa <105145167+fontesLarissa@users.noreply.github.com>
2023-07-03 01:12:28 -0300

    término do js adicionando troca de imagem de perfil ao trocar o modo

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if(html.classList.contains('light')) {
-  //   html.classList.remove('light')
-  // } else {
-  //   html.classList.add('light')
-  // }
-
   html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir imagem
+  if (html.classList.contains("light")) {
+    //se tiver ligh mode, add imagem light
+    img.setAttribute("src", "./assets/light-avatar.png")
+  } else {
+    //se tiver sem light  mode, manter a imagem
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
 }
